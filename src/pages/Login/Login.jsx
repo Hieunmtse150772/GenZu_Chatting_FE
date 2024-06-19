38e1bfdfc9eda3b55dcc5c56f5c6a639cea212a1
thienhoang241299 <thienhoang241299@gmail.com>
2024-06-19 14:42:01 +0700

    update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,18 +7,18 @@ export default function Login() {
   const { user } = useUser();
 
   return (
-    <div>
-      <div></div>
-    <div>
-      <SignedOut>
-        <SignInPage />
-      </SignedOut>
-      <SignedIn>
-        <p>Welcome, {user?.fullName}!</p>
-        <p>Email: {user?.primaryEmailAddress?.emailAddress}</p>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <div className="flex bg-mainBlue my-auto mx-auto w-2/4 justify-between items-center p-8">
+      <div>sad</div>
+      <div>
+        <SignedOut>
+          <SignInPage />
+        </SignedOut>
+        <SignedIn>
+          <p>Welcome, {user?.fullName}!</p>
+          <p>Email: {user?.primaryEmailAddress?.emailAddress}</p>
+          <UserButton />
+        </SignedIn>
+      </div>
     </div>
   );
 }
