87e4a3d7e15eb031c2cf1982797945e302c2b85d
thienhoang241299 <thienhoang241299@gmail.com>
2024-06-19 12:18:32 +0700

    setup enviroment and add login

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { ClerkProvider } from "@clerk/clerk-react";
+
+import ReactDOM from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env
@@ -13,9 +14,9 @@ if (!PUBLISHABLE_KEY) {
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+       <BrowserRouter>
+        <App />
+        </BrowserRouter>
     </ClerkProvider>
-  </React.StrictMode>
 );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 }
