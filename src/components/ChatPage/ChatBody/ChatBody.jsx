b4f4c210b98686049b665b1936b8ebd9ef9aaf9f
silver12024 <ngan12024@gmail.com>
2024-06-20 14:40:53 +0700

    update delete view component

--- src/components/ChatPage/ChatBody/ChatBody.jsx
+++ src/components/ChatPage/ChatBody/ChatBody.jsx
@@ -1,116 +1,81 @@
-import ChatHeader from "../ChatHeader/ChatHeader";
-import ChatFooter from "../ChatFooter/ChatFooter";
-import "./ChatBody.css";
+import ChatHeader from '../ChatHeader/ChatHeader'
+import ChatFooter from '../ChatFooter/ChatFooter'
+import DetailMessage from './DetailMessage/DetailMessage'
 
 function ChatBody() {
+  let message = [
+    {
+      id: 1,
+      message: "Hey, how's your day going?",
+      time: '',
+    },
+    {
+      id: 2,
+      message: 'Not too bad, just a bit busy. How about you?',
+      time: '',
+    },
+    {
+      id: 1,
+      message: "I'm good, thanks. Anything exciting happening?",
+      time: '',
+    },
+    {
+      id: 2,
+      message: "I'm good, thanks. Anything exciting happening?",
+      time: '',
+    },
+    {
+      id: 1,
+      message: 'Not really, just the usual. Work and errands.',
+      time: '',
+    },
+    {
+      id: 2,
+      message: 'Sounds like a typical day. Got any plans for the weekend?',
+      time: '',
+    },
+    {
+      id: 1,
+      message: "Not yet, I'm hoping to relax and maybe catch up on some reading. How about you?",
+      time: '',
+    },
+    {
+      id: 2,
+      message: "I might go hiking if the weather's nice. Otherwise, just taking it easy",
+      time: '',
+    },
+    {
+      id: 1,
+      message: 'Hiking sounds fun. Hope the weather cooperates for you!',
+      time: '',
+    },
+    {
+      id: 2,
+      message: 'You too, take care!',
+      time: '',
+    },
+    {
+      id: 1,
+      message: ' Sure',
+      time: '',
+    },
+    {
+      id: 2,
+      message: 'Thanks',
+      time: '',
+    },
+  ]
   return (
-    <div className="flex flex-col h-screen mx-2 w-full">
+    <div className='flex flex-col h-screen mx-2 w-full'>
       <ChatHeader />
-      <div className="flex flex-grow flex-col space-y-2 overflow-y-auto no-scrollbar">
+      <div className='flex flex-grow flex-col space-y-2 overflow-y-auto no-scrollbar'>
         {/* <!-- Messages go here -->
                 <!-- Example Message --> */}
-        <div className="flex justify-end">
-          <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-            Hey, how's your day going?
-          </div>
-        </div>
-
-        {/* <!-- Example Received Message --> */}
-        <div className="flex">
-          <div className="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-            Not too bad, just a bit busy. How about you?
-          </div>
-        </div>
-        {/* <!-- Example Message --> */}
-        <div className="flex justify-end">
-          <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-            I'm good, thanks. Anything exciting happening?
-          </div>
-        </div>
-
-        {/* <!-- Example Received Message --> */}
-        <div className="flex">
-          <div className="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-            Not really, just the usual. Work and errands.
-          </div>
-        </div>
-        {/* <!-- Example Message --> */}
-        <div className="flex justify-end">
-          <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-            Sounds like a typical day. Got any plans for the
-            weekend?
-          </div>
-        </div>
-
-        {/* <!-- Example Received Message --> */}
-        <div className="flex">
-          <div className="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-            Not yet, I'm hoping to relax and maybe catch up
-            on some reading. How about you?
-          </div>
-        </div>
-        {/* <!-- Example Message --> */}
-        <div className="flex justify-end">
-          <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-            I might go hiking if the weather's nice.
-            Otherwise, just taking it easy
-          </div>
-        </div>
-
-        {/* <!-- Example Received Message --> */}
-        <div className="flex">
-          <div className="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-            Hiking sounds fun. Hope the weather cooperates
-            for you!
-          </div>
-        </div>
-        {/* <!-- Example Message --> */}
-        <div className="flex justify-end">
-          <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-            Thanks! Fingers crossed. Enjoy your day!
-          </div>
-        </div>
-
-        {/* <!-- Example Received Message --> */}
-        <div className="flex">
-          <div className="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-            You too, take care!
-          </div>
-        </div>
-        {/* <!-- Example Message --> */}
-        <div className="flex justify-end">
-          <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-            Sure
-          </div>
-        </div>
-        <div className="flex justify-end">
-          <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-            Thanks
-          </div>
-        </div>
-
-        <div className="flex justify-end">
-          <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-            😁
-          </div>
-        </div>
-        {/* <!-- Example Message --> */}
-        <div className="flex">
-          <div className="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-            Okay
-          </div>
-        </div>
-
-        {/* <!-- Example Received Message --> */}
-        <div className="flex">
-          <div className="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-            😄
-          </div>
-        </div>
+        <DetailMessage inforMessage={message} />
       </div>
       <ChatFooter />
     </div>
-  );
+  )
 }
 
-export default ChatBody;
+export default ChatBody
---
+++ src/components/ChatPage/ChatBody/DetailMessage/DetailMessage.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+export default function DetailMessage(props) {
+  return (
+    <div>
+      {props.inforMessage.map((item, index) =>
+        item.id == 1 ? (
+          <div key={index} className='flex justify-end'>
+            <div className='bg-blue-200 text-black p-2 rounded-lg max-w-xs'>{item.message}</div>
+          </div>
+        ) : (
+          <div key={index} className='flex'>
+            <div className='bg-gray-300 text-black p-2 rounded-lg max-w-xs'>{item.message}</div>
+          </div>
+        ),
+      )}
+    </div>
+  )
+}
