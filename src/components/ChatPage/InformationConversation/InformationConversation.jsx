b3a4c9537e5dbf54507e2722f6eb433b21842fbf
huynth1801 <huuhuy1801@gmail.com>
2024-06-20 15:39:35 +0700

    feat: add new dropdown menu show info user

--- src/components/ChatPage/InformationConversation/InformationConversation.jsx
+++ src/components/ChatPage/InformationConversation/InformationConversation.jsx
@@ -1,10 +1,12 @@
 import { IoIosSearch } from 'react-icons/io'
 import { FaRegImage } from 'react-icons/fa'
 import { SlOptions } from 'react-icons/sl'
+import { MdOutlineGTranslate } from 'react-icons/md'
+import DropdownInfoItem from './DropdownInfoItem'
 
 function InformationConversation() {
   return (
-    <div className='max-w-2xl mx-auto bg-mainBlue'>
+    <div className='max-w-2xl mx-auto bg-mainBlue h-screen'>
       <div className='flex flex-col items-center pb-10'>
         <img
           className='mb-3 w-24 h-24 rounded-full shadow-lg'
@@ -20,50 +22,11 @@ function InformationConversation() {
           View profile
         </a>
         <div>
-          <ul className=' flex-col hidden md:flex overflow-x-hidden mx-2 rounded-lg font-semibold bg-white'>
-            <li className='p-1'>
-              <button
-                id='setting'
-                className='flex items-center cursor-pointer relative mb-4 hover:bg-mainBlue rounded-md p-1'
-                style={{ width: '100%' }}
-              >
-                <IoIosSearch size={24} />
-                <span className='ml-3 text-gray-900 text-sm font-medium'> Search chat</span>
-              </button>
-            </li>
-            <li className='p-1'>
-              <button
-                id='setting'
-                className='flex items-center cursor-pointer relative mb-4 hover:bg-mainBlue rounded-md p-1'
-                style={{ width: '100%' }}
-              >
-                <FaRegImage size={24} />
-                <span className='ml-3 text-gray-900 text-sm font-medium'> Sent images</span>
-              </button>
-            </li>
-            <li className='p-1'>
-              <label
-                for='toggle-example'
-                className='flex items-center cursor-pointer relative mb-4'
-              >
-                <input type='checkbox' className='sr-only' />
-                <div
-                  className='toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full'
-                  style={{ width: '30px' }}
-                ></div>
-                <span className='ml-3 text-gray-900 text-sm font-medium'>Auto translation</span>
-              </label>
-            </li>
-            <li className='p-1'>
-              <button
-                id='setting'
-                style={{ width: '100%' }}
-                className='flex items-center cursor-pointer relative mb-4 hover:bg-mainBlue rounded-md p-1'
-              >
-                <SlOptions size={20} />
-                <span className='ml-3 text-gray-900 text-sm font-medium'> More Option</span>
-              </button>
-            </li>
+          <ul className=' flex-col hidden md:flex overflow-x-hidden mx-2 px-6 py-2 rounded-lg font-semibold bg-white'>
+            <DropdownInfoItem icon={IoIosSearch} label={'Search chat'} />
+            <DropdownInfoItem icon={FaRegImage} label={'List of images'} />
+            <DropdownInfoItem icon={MdOutlineGTranslate} label={'Auto translate'} />
+            <DropdownInfoItem icon={SlOptions} label={'More Option'} />
           </ul>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,15 @@ import Sidebar from '../components/Sidebar/Sidebar'
 import { useUser, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
 import './Home.css'
 import SignInPage from './Login/SignInPage/SignInPage'
+import { useState } from 'react'
+import InformationConversation from '../components/ChatPage/InformationConversation/InformationConversation'
 
 export default function Home() {
+  const [showInfo, setShowInfo] = useState(false)
+
+  const toggleInfo = () => {
+    setShowInfo(!showInfo)
+  }
   return (
     <div className='fixed w-full'>
       <div className='Login'>
@@ -17,7 +24,12 @@ export default function Home() {
         <UserButton /> */}
           <main className='flex'>
             <Sidebar />
-            <ChatBody />
+            <ChatBody toggleInfo={toggleInfo} />
+            {showInfo && (
+              <div className='w-1/3'>
+                <InformationConversation />
+              </div>
+            )}
           </main>
         </SignedIn>
       </div>
