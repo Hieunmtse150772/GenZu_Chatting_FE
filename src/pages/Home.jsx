57c79dcffd00b239270143530715846c61e5a8a8
thienhoang241299 <thienhoang241299@gmail.com>
2024-06-20 11:47:19 +0700

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import ChatBody from "../components/View/ChatBody";
 import Sidebar from "../components/Sidebar/Sidebar";
+import { useUser, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+import "./Home.css";
+import SignInPage from "./Login/SignInPage/SignInPage";
+import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex">
-      <Sidebar />
-      <ChatBody />
-    </main>
+    <div className="Login">
+      <SignedOut>
+        <SignInPage />
+      </SignedOut>
+      <SignedIn>
+        {/* <p>Welcome, {user?.fullName}!</p>
+        <p>Email: {user?.primaryEmailAddress?.emailAddress}</p>
+        <UserButton /> */}
+        <main className="flex">
+          <Sidebar />
+          <ChatBody />
+        </main>
+      </SignedIn>
+    </div>
   );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useUser, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 import SignInPage from "./SignInPage/SignInPage";
 import { Routes, Route } from "react-router-dom";
 import SignUpComponent from "./SignUpPage/SignUpPage";
+import "./Login.scss";
 // import  { useEffect } from 'react';
 // import { useClerk } from '@clerk/clerk-react';
 
@@ -9,17 +10,13 @@ export default function Login() {
   const { user } = useUser();
 
   return (
-    <div className="flex bg-mainBlue my-auto mx-auto w-2/4 justify-between items-center p-8">
-      <div>
-        <img src="/src/assets/Lovepik_com-401526386-galaxy.png" alt="load image error" />
-      </div>
+    <div className="Login flex bg-mainBlue my-auto mx-auto w-screen items-center justify-center h-screen">
       <div>
         <SignedOut>
           <Routes>
             <Route path="/" element={<SignInPage />} />
-            <Route path="/signup" element={<SignUpComponent />} />
+            <Route path="signup" element={<SignUpComponent />} />
           </Routes>
-          {/* <SignInPage /> */}
         </SignedOut>
         <SignedIn>
           <p>Welcome, {user?.fullName}!</p>
