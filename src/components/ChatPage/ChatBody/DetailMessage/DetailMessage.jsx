4e591f33c24793508effde85f7a01105fee68f75
huynth1801 <huuhuy1801@gmail.com>
2024-06-20 16:03:19 +0700

    fix: fix sidebar scroll width

--- src/components/ChatPage/ChatBody/DetailMessage/DetailMessage.jsx
+++ src/components/ChatPage/ChatBody/DetailMessage/DetailMessage.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable react/prop-types */
 export default function DetailMessage(props) {
   return (
-    <div>
+    <div className='mx-2'>
       {props.inforMessage.map((item, index) =>
         item.id == 1 ? (
           <div key={index} className='flex justify-end'>
-            <div className='bg-blue-200 text-black p-2 rounded-lg max-w-xs'>{item.message}</div>
+            <div className='max-w-xs rounded-lg bg-blue-200 p-2 text-black'>{item.message}</div>
           </div>
         ) : (
           <div key={index} className='flex'>
-            <div className='bg-gray-300 text-black p-2 rounded-lg max-w-xs'>{item.message}</div>
+            <div className='max-w-xs rounded-lg bg-gray-300 p-2 text-black'>{item.message}</div>
           </div>
         ),
       )}
--- src/components/ChatPage/ChatHeader/ChatHeader.jsx
+++ src/components/ChatPage/ChatHeader/ChatHeader.jsx
@@ -4,22 +4,25 @@ import { MdPhone, MdVideocam } from 'react-icons/md'
 
 function ChatHeader({ toggleInfo }) {
   return (
-    <header className='flex justify-between items-center py-4 bg-mainBlue px-8 rounded-lg mb-2'>
-      <div className='flex items-center space-x-4 cursor-pointer'>
-        <RxAvatar size={40} className='text-orange-500' />
+    <header className='mb-2 flex items-center justify-between rounded-lg bg-mainBlue px-8 py-4 shadow-xl'>
+      <div className='flex cursor-pointer items-center space-x-4'>
+        <img
+          src='https://flowbite.com/docs/images/people/profile-picture-3.jpg'
+          className='h-16 w-16 rounded-full'
+        />
         <div className='flex flex-col'>
-          <span className='text-2xl text-black font-semibold'>Username</span>
-          <span className='text-base text-gray-500 font-semibold'>Active 2h ago</span>
+          <span className='text-2xl font-semibold text-black'>Username</span>
+          <span className='text-base font-semibold text-gray-500'>Active 2h ago</span>
         </div>
       </div>
       <div className='flex space-x-6'>
-        <button className='hover:bg-blue-400 rounded-md p-2'>
+        <button className='rounded-md p-2 hover:bg-blue-400'>
           <MdPhone size={22} />
         </button>
-        <button className='hover:bg-blue-400 rounded-md p-2'>
+        <button className='rounded-md p-2 hover:bg-blue-400'>
           <MdVideocam size={22} />
         </button>
-        <button className='hover:bg-blue-400 rounded-md p-2' onClick={toggleInfo}>
+        <button className='rounded-md p-2 hover:bg-blue-400' onClick={toggleInfo}>
           <SlOptions size={22} />
         </button>
       </div>
