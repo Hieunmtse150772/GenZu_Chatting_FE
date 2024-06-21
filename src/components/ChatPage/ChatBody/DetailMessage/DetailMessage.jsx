035888d783d43636950d2764acbac57a3dff0bfc
silver12024 <ngan12024@gmail.com>
2024-06-21 13:52:39 +0700

    modify hover message sẽ hiện featureAI

--- src/components/ChatPage/ChatBody/DetailMessage/DetailMessage.jsx
+++ src/components/ChatPage/ChatBody/DetailMessage/DetailMessage.jsx
@@ -1,40 +1,53 @@
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect } from "react";
 import FeatureAI from '../FeatureAI/FeatureAI'
 import { useSelector } from 'react-redux'
 /* eslint-disable react/prop-types */
 export default function DetailMessage(props) {
-  const [isOptionBtnClick, setIsOptionBtnClick] = useState(false)
+  const [isOptionBtnClick, setIsOptionBtnClick] =
+    useState(false);
+  
+  const [activeMessageID, setActiveMessageID] = useState(null);
 
-  const dropdownRef = useRef(null)
+  const handleUserClick = (id) => {
+    setActiveMessageID(id);
+  };
+  const dropdownRef = useRef(null);
   const message = useSelector((state) => state.message.message)
-  // const handleMoreClick = (e) => {
+  // const handleMoreClick = (id) => {
   //   e.preventDefault();
   //   setIsOptionBtnClick(!isOptionBtnClick);
+  //   if(setIsOptionBtnClick){
+  //     handleUserClick(id)
+  //   }
   // };
 
   return (
     <div className='mx-2'>
       {message.map((item, index) =>
-        item.id == 1 ? (
+        item.id_user == 1 ? (
           <div key={index} className='flex justify-end'>
             <div className='my-4 max-w-xs rounded-lg bg-blue-200 p-2 text-black'>
               {item.message}
             </div>
           </div>
         ) : (
-          <div key={index} className='flex'>
-            <div
-              className='max-w-xs rounded-lg bg-gray-300 p-2 text-black'
-              onMouseEnter={() => setIsOptionBtnClick(true)}
-              onMouseLeave={() => setIsOptionBtnClick(false)}
-            >
-              {item.message}
+          <div key={index} className='flex' 
+                onMouseEnter={() => {setIsOptionBtnClick(true)
+                  handleUserClick(item.id_message)
+                }}
+                onMouseLeave={() => setIsOptionBtnClick(false)}>
+            <div className='max-w-xs rounded-lg bg-gray-300 p-2 text-black' 
+                >
+                    {item.message}
             </div>
             <div
-              className={`${
-                isOptionBtnClick ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
-              }`}
-            >
+                className={`${
+                  isOptionBtnClick && activeMessageID == item.id_message
+                    ? "opacity-100"
+                    : "opacity-0 group-hover:opacity-100"
+                }`}
+                
+              >
               <FeatureAI />
             </div>
           </div>
--- src/redux/Slice/messageSlice.js
+++ src/redux/Slice/messageSlice.js
@@ -3,62 +3,74 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
   message: [
     {
-      id: 1,
+      id_user: 1,
+      id_message:1,
       message: "Hey, how's your day going?",
       time: '',
     },
     {
-      id: 2,
+      id_user: 2,
+      id_message:2,
       message: 'Not too bad, just a bit busy. How about you?',
       time: '',
     },
     {
-      id: 1,
+      id_user: 1,
+      id_message:3,
       message: "I'm good, thanks. Anything exciting happening?",
       time: '',
     },
     {
-      id: 2,
+      id_user: 2,
+      id_message:4,
       message: "I'm good, thanks. Anything exciting happening?",
       time: '',
     },
     {
-      id: 1,
+      id_user: 1,
+      id_message:5,
       message: 'Not really, just the usual. Work and errands.',
       time: '',
     },
     {
-      id: 2,
+      id_user: 2,
+      id_message:6,
       message: 'Sounds like a typical day. Got any plans for the weekend?',
       time: '',
     },
     {
-      id: 1,
+      id_user: 1,
+      id_message:7,
       message: "Not yet, I'm hoping to relax and maybe catch up on some reading. How about you?",
       time: '',
     },
     {
-      id: 2,
+      id_user: 2,
+      id_message:8,
       message: "I might go hiking if the weather's nice. Otherwise, just taking it easy",
       time: '',
     },
     {
-      id: 1,
+      id_user: 1,
+      id_message:9,
       message: 'Hiking sounds fun. Hope the weather cooperates for you!',
       time: '',
     },
     {
-      id: 2,
+      id_user: 2,
+      id_message:10,
       message: 'You too, take care!',
       time: '',
     },
     {
-      id: 1,
+      id_user: 1,
+      id_message:11,
       message: ' Sure',
       time: '',
     },
     {
-      id: 2,
+      id_user: 2,
+      id_message:12,
       message: 'Thanks',
       time: '',
     },
@@ -72,7 +84,7 @@ const messageSlice = createSlice({
       console.log(action.payload)
       return {
         ...state,
-        message: [...state.message, { id: 1, message: action.payload, time: '' }],
+        message: [...state.message, { id_user: 1, id_message:1, message: action.payload, time: '' }],
       }
     },
     setMessage: (state) => {
