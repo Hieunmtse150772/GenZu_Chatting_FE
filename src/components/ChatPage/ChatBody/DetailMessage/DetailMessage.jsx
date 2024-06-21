eebdcbbbca6372f2e461c3dcaa823db343a1c671
thienhoang241299 <thienhoang241299@gmail.com>
2024-06-21 11:05:04 +0700

    update chat

--- src/components/ChatPage/ChatBody/DetailMessage/DetailMessage.jsx
+++ src/components/ChatPage/ChatBody/DetailMessage/DetailMessage.jsx
@@ -1,12 +1,12 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect } from 'react'
 import FeatureAI from '../FeatureAI/FeatureAI'
+import { useSelector } from 'react-redux'
 /* eslint-disable react/prop-types */
 export default function DetailMessage(props) {
-  const [isOptionBtnClick, setIsOptionBtnClick] =
-    useState(false);
+  const [isOptionBtnClick, setIsOptionBtnClick] = useState(false)
 
-  const dropdownRef = useRef(null);
-
+  const dropdownRef = useRef(null)
+  const message = useSelector((state) => state.message.message)
   // const handleMoreClick = (e) => {
   //   e.preventDefault();
   //   setIsOptionBtnClick(!isOptionBtnClick);
@@ -14,7 +14,7 @@ export default function DetailMessage(props) {
 
   return (
     <div className='mx-2'>
-      {props.inforMessage.map((item, index) =>
+      {message.map((item, index) =>
         item.id == 1 ? (
           <div key={index} className='flex justify-end'>
             <div className='my-4 max-w-xs rounded-lg bg-blue-200 p-2 text-black'>
@@ -23,19 +23,18 @@ export default function DetailMessage(props) {
           </div>
         ) : (
           <div key={index} className='flex'>
-            <div className='max-w-xs rounded-lg bg-gray-300 p-2 text-black' 
-                onMouseEnter={() => setIsOptionBtnClick(true)}
-                onMouseLeave={() => setIsOptionBtnClick(false)}>
-                    {item.message}
+            <div
+              className='max-w-xs rounded-lg bg-gray-300 p-2 text-black'
+              onMouseEnter={() => setIsOptionBtnClick(true)}
+              onMouseLeave={() => setIsOptionBtnClick(false)}
+            >
+              {item.message}
             </div>
             <div
-                className={`${
-                  isOptionBtnClick
-                    ? "opacity-100"
-                    : "opacity-0 group-hover:opacity-100"
-                }`}
-                
-              >
+              className={`${
+                isOptionBtnClick ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
+              }`}
+            >
               <FeatureAI />
             </div>
           </div>
