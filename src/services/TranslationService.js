be0bc5a64b4015cf653ed1256bd00ad12c2a1413
thienhoang241299 <thienhoang241299@gmail.com>
2024-06-24 11:02:47 +0700

    upload translationService file

---
+++ src/services/TranslationService.js
@@ -0,0 +1,26 @@
+// src/services/translationService.js
+import axios from 'axios'
+
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY
+const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`
+
+export const translateText = async (
+  text = 'GenzuChat Là số 1 trên thế giới',
+  targetLang = 'en',
+) => {
+  try {
+    const response = await axios.post(url, {
+      q: text,
+      target: targetLang,
+    })
+
+    if (response.data.data && response.data.data.translations) {
+      return response.data.data.translations[0].translatedText
+    } else {
+      throw new Error('Unexpected response format from translation service')
+    }
+  } catch (error) {
+    console.error('Error translating text:', error)
+    throw error // Rethrow the error to propagate it further
+  }
+}
