14c3eee05763ddac04c6b0de578ee57502060c84
silver12024 <ngan12024@gmail.com>
2024-06-26 11:53:59 +0700

    update emoji messageSlice

--- src/redux/Slice/messageSlice.js
+++ src/redux/Slice/messageSlice.js
@@ -7,72 +7,105 @@ const initialState = {
       id_message: 1,
       message: "Hey, how's your day going?",
       time: '',
+      emoji_user:[
+        {
+          id_user:1,
+          url_emoji:"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/1f603.png"
+        },
+        {
+          id_user:2,
+          url_emoji:"https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/1f603.png"
+        }
+      ]
     },
     {
       id_user: 2,
       id_message: 2,
       message: 'Not too bad, just a bit busy. How about you?',
       time: '',
+      emoji_user:[
+      ]
     },
     {
       id_user: 1,
       id_message: 3,
       message: "I'm good, thanks. Anything exciting happening?",
       time: '',
+      emoji_user:[
+        
+       ]
     },
     {
       id_user: 2,
       id_message: 4,
       message: "I'm good, thanks. Anything exciting happening?",
       time: '',
+      emoji_user:[
+       ]
     },
     {
       id_user: 1,
       id_message: 5,
       message: 'Not really, just the usual. Work and errands.',
       time: '',
+      emoji_user:[
+        ]
     },
     {
       id_user: 2,
       id_message: 6,
       message: 'Sounds like a typical day. Got any plans for the weekend?',
       time: '',
+      emoji_user:[
+       ]
     },
     {
       id_user: 1,
       id_message: 7,
       message: "Not yet, I'm hoping to relax and maybe catch up on some reading. How about you?",
       time: '',
+      emoji_user:[
+        ]
     },
     {
       id_user: 2,
       id_message: 8,
       message: "I might go hiking if the weather's nice. Otherwise, just taking it easy",
       time: '',
+      emoji_user:[
+        ]
     },
     {
       id_user: 1,
       id_message: 9,
       message: 'Hiking sounds fun. Hope the weather cooperates for you!',
       time: '',
+      emoji_user:[
+        ]
     },
     {
       id_user: 2,
       id_message: 10,
       message: 'You too, take care!',
       time: '',
+      emoji_user:[
+       ]
     },
     {
       id_user: 1,
       id_message: 11,
       message: ' Sure',
       time: '',
+      emoji_user:[
+        ]
     },
     {
       id_user: 2,
       id_message: 12,
       message: 'Thanks',
       time: '',
+      emoji_user:[
+        ]
     },
   ],
   selectedEmojis: [],
@@ -82,7 +115,7 @@ const messageSlice = createSlice({
   initialState,
   reducers: {
     sendMessage: (state, action) => {
-      return {
+            return {
         ...state,
         message: [
           ...state.message,
@@ -102,8 +135,25 @@ const messageSlice = createSlice({
     deleteEmoji: (state) => {
       return { ...state, selectedEmojis: [] }
     },
+    selectedEmjiOnMessage: (state, action) => {
+      console.log('emoji_payload:', action.payload);
+
+      state.message = [
+        ...state.message,
+      ]
+      state.message.map((item, index) =>{
+        if(item.id_message == action.payload.id_message){
+          state.message[index].emoji_user = [ 
+            ...state.message[index].emoji_user,
+            { id_user: action.payload.id_user, url_emoji: action.payload.url},
+          ]
+        }
+      }
+      )
+      console.log('state:', state.message)
+  },
   },
 })
 
-export const { sendMessage, setMessage, selectEmoji, deleteEmoji } = messageSlice.actions
+export const { sendMessage, setMessage, selectEmoji, deleteEmoji, selectedEmjiOnMessage } = messageSlice.actions
 export default messageSlice.reducer
