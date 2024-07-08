13bb785723ae6823785dc3e7a6c05ece3b45b795
huynth1801 <huuhuy1801@gmail.com>
2024-07-08 10:14:07 +0700

    reply friend request notification

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import ChatBody from '../../components/ChatPage/ChatBody/ChatBody'
 import ToastMessage from '../../components/ToastMessage/ToastMessage'
@@ -13,21 +13,44 @@ import {
   getLsConversation,
   setConversation,
   setIdConversation,
+  setToastMessage,
 } from '@/redux/Slice/userSlice'
 import { connectSocket } from '@/redux/Slice/chatSlice'
+import { getCookie } from '@/services/Cookies'
 import LoadingSpinner from './ChatSkeleton/ChatSkeleton'
 
 export default function Chat() {
   const [showInfo, setShowInfo] = useState(false)
   const dispatch = useDispatch()
   const toastMessage = useSelector((state) => state?.user?.toastMessage)
-  const getStatusFriendRequest = useSelector((state) => state?.user?.friendRequestNotification).at(
-    -1,
-  )
+  const friendRequestNotification =
+    useSelector((state) => state?.user?.friendRequestNotification) || []
+  const getStatusFriendRequest = friendRequestNotification.length
+    ? friendRequestNotification.at(-1)
+    : null
+  const session = Object.values(JSON.parse(getCookie('userLogin')))
+  // Lấy id của session user
+  const sessionId = Object.keys(session).map((key) => {
+    return session[key]._id
+  })[2]
   const navigate = useNavigate()
   const idConversation = useParams()
   const conversation = useSelector((state) => state.user.conversation)
   const lsConversation = useSelector((state) => state.user.lsConversation)
+  const toastRef = useRef(null)
+
+  useEffect(() => {
+    if (getStatusFriendRequest?.sender?._id === sessionId) {
+      if (getStatusFriendRequest?.status === 'accepted' && toastRef.current !== 'accepted') {
+        dispatch(setToastMessage('Your friend request was accepted'))
+        toastRef.current = 'accepted'
+      } else if (getStatusFriendRequest?.status === 'declined' && toastRef.current !== 'declined') {
+        dispatch(setToastMessage('Your friend request was declined'))
+        toastRef.current = 'declined'
+      }
+    }
+  }, [getStatusFriendRequest, dispatch, sessionId])
+
   const toggleInfo = () => {
     setShowInfo(!showInfo)
   }
@@ -53,6 +76,7 @@ export default function Chat() {
     if (toastMessage) {
       const timer = setTimeout(() => {
         dispatch(clearToastMessage()) // Clear the toast message after 2 seconds
+        toastRef.current = null // Reset the toastRef after clearing the message
       }, 2000)
       return () => clearTimeout(timer) // Cleanup the timer on unmount
     }
@@ -62,7 +86,6 @@ export default function Chat() {
     <div className='fixed w-full'>
       <div className='Login relative'>
         <main className='flex'>
-          {console.log('get last status', getStatusFriendRequest)}
           <Sidebar />
           {conversation ? <ChatBody toggleInfo={toggleInfo} /> : <LoadingSpinner />}
           {showInfo && (
