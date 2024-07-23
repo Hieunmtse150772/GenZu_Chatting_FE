b07362dc3de58be1159bafb604e17b6d3688ef29
huynth1801 <huuhuy1801@gmail.com>
2024-07-23 11:30:13 +0700

    fix dark mode

--- src/components/ChatPage/ChatBody/SearchMessage/SearchMessage.jsx
+++ src/components/ChatPage/ChatBody/SearchMessage/SearchMessage.jsx
@@ -1,4 +1,3 @@
-
 import { IoIosSearch, IoIosClose, IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
 import { updateStateSearch } from '@/redux/Slice/messageSlice'
 import { useDispatch, useSelector } from 'react-redux'
@@ -6,64 +5,67 @@ import { useEffect, useState } from 'react'
 import { searchMessageByKeyword } from '@/redux/Slice/chatSlice'
 // import { searchMessage } from '@/redux/Slice/messageSlice'
 
-export default function SearchMessage(props){
-    const resultMessage = useSelector((state) => state.message.resultMessage)
-    const [index, setIndex] = useState()
-    const [inputStr, setInputStr] = useState('')
-    const dispatch = useDispatch()
-    // Xử lý đóng search message
-    const handleCloseBtn = (e) =>{
-        dispatch(updateStateSearch(false))
+export default function SearchMessage(props) {
+  const resultMessage = useSelector((state) => state.message.resultMessage)
+  const [index, setIndex] = useState()
+  const [inputStr, setInputStr] = useState('')
+  const dispatch = useDispatch()
+  // Xử lý đóng search message
+  const handleCloseBtn = (e) => {
+    dispatch(updateStateSearch(false))
+  }
+  const handleDeleteStr = (e) => {
+    setInputStr('')
+  }
+  // Hàm xử lý khi nội dung input field thay đổi
+  const handleChangeInput = (e) => {
+    setInputStr(e.target.value)
+  }
+  // Xử lý sự kiện nhấn phím Enter trong input field
+  const handleKeyPress = (e) => {
+    // Kiểm tra nếu phím Enter được nhấn
+    if (e.keyCode === 13) {
+      // Gửi tin nhắn
+      handleSearchMsg()
     }
-    const handleDeleteStr = (e) =>{
-        setInputStr('')
+  }
+  // Hàm xử lý gửi tin nhắn
+  const handleSearchMsg = () => {
+    // Kiểm tra text khác null, undefined và rỗng
+    if (inputStr != null && inputStr != undefined && inputStr != '') {
+      const searchItem = {
+        idConversation: props.idConversation,
+        keyword: inputStr,
+      }
+      // Dispatch action gửi tin nhắn
+      dispatch(searchMessageByKeyword(searchItem))
     }
-    // Hàm xử lý khi nội dung input field thay đổi
-    const handleChangeInput = (e) => {
-        setInputStr(e.target.value)
-    }
-     // Xử lý sự kiện nhấn phím Enter trong input field
-    const handleKeyPress = (e) => {
-        // Kiểm tra nếu phím Enter được nhấn
-        if (e.keyCode === 13) {
-        // Gửi tin nhắn
-        handleSearchMsg()
-        }
-    }
-    // Hàm xử lý gửi tin nhắn
-    const handleSearchMsg = () => {
-        // Kiểm tra text khác null, undefined và rỗng
-        if (inputStr != null && inputStr != undefined && inputStr != '') {
-        
-            const searchItem = {
-                idConversation: props.idConversation,
-                keyword: inputStr
-            }
-            // Dispatch action gửi tin nhắn
-            dispatch(searchMessageByKeyword(searchItem))
-            }
-        }
-    useEffect(() => {
-        setIndex(props.indexMgs)
-    }, [props.indexMgs])
-    return (
-            <div className='relative space-x-2 flex items-center rounded-lg bg-mainBlue px-4 pb-4 pt-1 dark:bg-darkBlack'>
-                <button className='items-center rounded-full  hover:bg-blue-400 dark:hover:bg-[#357ABD]'>
-                    <IoIosSearch size={24}/>
-                </button>
-                <input
-                    placeholder='Nhập tin nhắn...'
-                    onChange={handleChangeInput}
-                    onKeyDown={handleKeyPress}
-                    value={inputStr}
-                    className='flex-1 bg-white dark:bg-black dark:text-white rounded-full border px-4 py-2 focus:outline outline-cyan-600 mr-2'
-                />
-                { inputStr && <button title='Xóa'
-                        className='items-center rounded-md  hover:bg-blue-400 dark:hover:bg-[#357ABD]'
-                        onClick={handleDeleteStr}>
-                    <IoIosClose size={24}/>
-                </button>}
-                {/* {(resultMessage != undefined) && (index > 0 && index < resultMessage.length) 
+  }
+  useEffect(() => {
+    setIndex(props.indexMgs)
+  }, [props.indexMgs])
+  return (
+    <div className='relative flex items-center space-x-2 rounded-lg bg-mainBlue px-4 pb-4 pt-1 dark:bg-darkBlack'>
+      <button className='items-center rounded-full hover:bg-blue-400 dark:hover:bg-[#357ABD]'>
+        <IoIosSearch size={24} />
+      </button>
+      <input
+        placeholder='Nhập tin nhắn...'
+        onChange={handleChangeInput}
+        onKeyDown={handleKeyPress}
+        value={inputStr}
+        className='mr-2 flex-1 rounded-full border bg-white px-4 py-2 outline-cyan-600 focus:outline dark:bg-black dark:text-white'
+      />
+      {inputStr && (
+        <button
+          title='Xóa'
+          className='items-center rounded-md hover:bg-blue-400 dark:hover:bg-[#357ABD]'
+          onClick={handleDeleteStr}
+        >
+          <IoIosClose size={24} />
+        </button>
+      )}
+      {/* {(resultMessage != undefined) && (index > 0 && index < resultMessage.length) 
                     ? 
                     (   <button className='items-center rounded-md  hover:bg-blue-400 dark:hover:bg-[#357ABD]'
                                 onClick={props.handleArrowUpBtn}>
@@ -72,8 +74,8 @@ export default function SearchMessage(props){
                     : ( <button type="button" className="text-gray-300" disabled>
                                     <IoIosArrowUp size={24}/>
                         </button>)} */}
-                
-                {/* {(resultMessage != undefined) && (index >= 0 && index < resultMessage.length-1) ? (<button className='items-center rounded-md  hover:bg-blue-400 dark:hover:bg-[#357ABD]'
+
+      {/* {(resultMessage != undefined) && (index >= 0 && index < resultMessage.length-1) ? (<button className='items-center rounded-md  hover:bg-blue-400 dark:hover:bg-[#357ABD]'
                                                             onClick={props.handleArrowDownBtn}>
                                                         <IoIosArrowDown size={24}/>
                                                     </button>)
@@ -81,11 +83,13 @@ export default function SearchMessage(props){
                                                         <IoIosArrowDown size={24}/>
                                                     </button>)}
                  */}
-                <button title='Đóng tìm kiếm'
-                        className='items-center rounded-ms  hover:bg-blue-400 dark:hover:bg-[#357ABD]'
-                        onClick={handleCloseBtn}>
-                    <span> Đóng</span>
-                </button>
-            </div>
-    )
+      <button
+        title='Đóng tìm kiếm'
+        className='rounded-ms items-center hover:bg-blue-400 dark:hover:bg-[#357ABD]'
+        onClick={handleCloseBtn}
+      >
+        <span className='dark:text-white'> Đóng</span>
+      </button>
+    </div>
+  )
 }
